f14f7d3e4a93db5a5e7a16ea3e8dab2fc8657f95
leobeltran <alpha059a@gmail.com>
2016-12-15 21:07:18 -0500

    added toyschema and connect to mlab DB

---
+++ index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Toy = require('./toy');
+
+//connect to DB = mlab mongoDB
+mongoose.connect('mongodb://santa:claus@ds133418.mlab.com:33418/christmas_list', (err) =>{
+  if (err) {
+    throw err;
+  }
+
+  // const toy = new Toy({
+  //   type: 'Bicycle',
+  //   description: 'Schwinn Trainer Bike',
+  //   gender: 'girl',
+  //   age: 4,
+  //   price: 60,
+  // });
+  //
+  // toy.save((err) => {
+  //   if (err) {
+  //     throw err;
+  //
+  //     console.log(toy);
+  //     process.exit();
+  //   };
+  // });
+
+});
+
+console.log('Process Complete!!');
--- toy.js
+++ toy.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ToySchema = new mongoose.Schema({
   type: { type: String },
+  description: { type: String },
   gender: { type: String },
   age: { type: Number },
   price: { type: Number },
