569aeeee1325dac3195cb5eeb18b9c1a13577213
leobeltran <alpha059a@gmail.com>
2016-12-17 07:51:17 -0500

    completed CRUD for toys

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-const Toy = require('./toy');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+//
+// const Toy = require('./toy');
 
 //connect to DB = mlab mongoDB
-mongoose.connect('mongodb://santa:claus@ds133418.mlab.com:33418/christmas_list', (err) =>{
-  if (err) {
-    throw err;
-  }
+// mongoose.connect('mongodb://santa:claus@ds133418.mlab.com:33418/christmas_list', (err) =>{
+//   if (err) {
+//     throw err;
+//   }
 
   // const toy = new Toy({
   //   type: 'Bicycle',
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://santa:claus@ds133418.mlab.com:33418/christmas_list',
   //     process.exit();
   //   };
   // });
-
-});
-
-console.log('Process Complete!!');
+//
+// });
+//
+// console.log('Process Complete!!');
--- server/server.js
+++ server/server.js
@@ -1,4 +1,63 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const _ = require('lodash');
-// const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+const app = express();
+
+//middleware
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let toys = [];
+let id = 0;
+
+// Create toy
+app.post('/toys', (req,res) => {
+  let toy = req.body;
+
+  id++
+  toys.id =id + '';
+  toys.push(toy);
+});
+
+// Read toys
+app.get('/toys', (req,res) => {
+  res.json(toys);
+});
+
+//Read one toy
+app.get('toys/:id', (req,res) => {
+  const toy = _.find(lions, {id: req.params.id});
+  res.json(toy || {});
+});
+
+// Update toy
+app.put('/toys/:id', (req,res) => {
+  let update =req.body;
+  if (update.id) { delete update.id }
+  let toy = _.findIndex(lions, {id: req.params.id});
+  if (!toys[toy]) {
+    res.send();
+  } else {
+    const updatLion = _.assign(toys[toy], update);
+    res.json(updateLion);
+  }
+});
+
+// Delete toy
+app.delete('/lions/:id', (req,res) => {
+  const lion = _.findIndex(toys, {id: req.params.id});
+  if(!toys[toy]) {
+    res.end();
+  } else {
+    const deleteToy = toys[toy];
+    lions.splice(lion, 1);
+    res.json(deleteToy);
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log('listening on http://localhost:', PORT);
+});
